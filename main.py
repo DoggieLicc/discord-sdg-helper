4b617d555833eab977b37286adcd3e428f2efa25
DoggieLicc <doggie@doggieli.cc>
2024-10-31 18:00:06 -0400

    Improve error handling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 import os
 import random
 import copy
+import traceback
 
 from dotenv import load_dotenv
 from discord import app_commands
@@ -302,13 +303,53 @@ async def on_raw_thread_delete(payload: discord.RawThreadDeleteEvent):
 
 @client.tree.error
 async def error_handler(interaction: discord.Interaction, error: app_commands.AppCommandError):
-    error_message = str(error)
+    error_message = None
 
     if isinstance(error, app_commands.CommandInvokeError):
-        original = error.original
+        error = error.original
 
-        if isinstance(original, app_commands.TransformerError):
-            error_message = 'Invalid option'
+    if isinstance(error, app_commands.TransformerError):
+        error_message = f'Invalid option "{error.value}" for {error.type}'
+
+    if isinstance(error, app_commands.CheckFailure):
+        error_message = 'You aren\'t allowed to use that command!'
+
+    if isinstance(error, app_commands.MissingPermissions):
+        error_message = f'You are missing the following permissions: {error.missing_permissions}'
+
+    if isinstance(error, app_commands.BotMissingPermissions):
+        error_message= f'The bot is missing the following permissions: {error.missing_permissions}'
+
+    if not error_message:
+        error_message = f'An unknown error occurred: {error}\n\nError info will be sent to owner'
+
+        etype = type(error)
+        trace = error.__traceback__
+        lines = traceback.format_exception(etype, error, trace)
+        traceback_t: str = ''.join(lines)
+
+        print(traceback_t)
+        file = utils.str_to_file(traceback_t, filename='traceback.py')
+
+        owner: discord.User = client.get_user(DEV)
+
+        if owner:
+            owner_embed = utils.create_embed(
+                interaction.user,
+                title='Unhandled error occurred!',
+                color=discord.Color.red()
+            )
+
+            owner_embed.add_field(name='Unhandled Error!:', value=f"Error {error}", inline=False)
+            owner_embed.add_field(name='Command:', value=str(interaction.data)[:1000], inline=False)
+
+            owner_embed.add_field(
+                name='Extra Info:',
+                value=f'Guild: {interaction.guild}: {getattr(interaction.guild, "id", "None")}\n'
+                      f'Channel: {interaction.channel.mention}:{interaction.channel.id}', inline=False
+            )
+
+            await owner.send(embed=owner_embed, files=[file])
 
     embed = utils.create_embed(
         interaction.user,
